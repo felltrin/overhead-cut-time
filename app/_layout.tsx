446b072879bb387f9db41187340662581dce13c2
Lucius Holzinger <luciholzinger@gmail.com>
2026-02-17 11:07:53 -0800

    moved apollo client into a seperate file to reduce understanding complexity

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,64 +14,8 @@ import { useAuthContext } from "@/hooks/use-auth-context";
 import { useColorScheme } from "@/hooks/use-color-scheme";
 import AuthProvider from "@/providers/auth-provider";
 
-import {
-  ApolloClient,
-  InMemoryCache,
-  HttpLink,
-  defaultDataIdFromObject,
-} from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
-import { relayStylePagination } from "@apollo/client/utilities";
-import { setContext } from "@apollo/client/link/context";
-import { supabase } from "@/lib/supabase";
-
-const cache = new InMemoryCache({
-  dataIdFromObject(responseObject) {
-    if ("nodeId" in responseObject) {
-      return `${responseObject.nodeId}`;
-    }
-
-    return defaultDataIdFromObject(responseObject);
-  },
-  possibleTypes: { Node: ["Todos"] },
-  typePolicies: {
-    Query: {
-      fields: {
-        todosCollection: relayStylePagination(),
-        node: {
-          read(_, { args, toReference }) {
-            const ref = toReference({
-              nodeId: args?.nodeId,
-            });
-
-            return ref;
-          },
-        },
-      },
-    },
-  },
-});
-
-const httpLink = new HttpLink({
-  uri: process.env.EXPO_PUBLIC_SUPABASE_PROJECT_URI ?? "",
-});
-
-const authLink = setContext(async (_, { header }) => {
-  const token = (await supabase.auth.getSession()).data.session?.access_token;
-
-  return {
-    headers: {
-      ...header,
-      Authorization: token ? `Bearer ${token}` : "",
-      apiKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache,
-});
+import client from "@/lib/apollo";
 
 // Separate RootNavigator so we can access the AuthContext
 function RootNavigator() {
